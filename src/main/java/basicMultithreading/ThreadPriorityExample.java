311c32285cc73e5f2210fb3b1cca7d62320c0b98
adarsh maharjan <adarsh986030@gmail.com>
2025-03-26 19:36:50 +0545

    chore: implemented thred priority

--- src/main/java/basicMultithreading/ThreadPriorityExample.java
+++ src/main/java/basicMultithreading/ThreadPriorityExample.java
@@ -2,9 +2,27 @@ package basicMultithreading;
 
 public class ThreadPriorityExample {
     public static void main(String[] args) {
+        // * example
+        // thredExecutionExample();
+
+        //
+        setExecutingThreadPriority();
+    }
+
+    static void thredExecutionExample() {
         System.out.println(Thread.currentThread().getName());
         System.out.println(Thread.currentThread().getPriority());
         Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
         System.out.println(Thread.currentThread().getPriority());
+
+    }
+
+    static void setExecutingThreadPriority() {
+        System.out.println(Thread.currentThread().getName() + " says HI");
+        Thread one = new Thread(() -> {
+            System.out.println("Thread one says hi also");
+        });
+        one.setPriority(Thread.MAX_PRIORITY);
+        one.start();
     }
 }
