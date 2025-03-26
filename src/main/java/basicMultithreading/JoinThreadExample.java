bfb36dce804516b38eda0ae3b3577120d012b6e3
adarsh maharjan <adarsh986030@gmail.com>
2025-03-26 17:36:41 +0545

    chore: implemnted thred join meathod

--- src/main/java/basicMultithreading/JoinThreadExample.java
+++ src/main/java/basicMultithreading/JoinThreadExample.java
@@ -1,23 +1,24 @@
 package basicMultithreading;
 
 public class JoinThreadExample {
+
     public static void main(String[] args) throws InterruptedException {
         Thread one = new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
-                System.out.println("Thread 1 : " + i);
+            for (int i = 0; i < 5; i += 1) {
+                System.out.println("Thread 1: " + i);
             }
         });
 
         Thread two = new Thread(() -> {
-            for (int i = 0; i < 25; i++) {
+            for (int i = 0; i < 25; i += 1) {
                 System.out.println("Thread 2 : " + i);
             }
         });
-
-        System.out.println("Before invoking threads...");
+        System.out.println("Before executing the threads");
         one.start();
         two.start();
         one.join();
-        System.out.println("Done executing the threads!");
+        two.join();
+        System.out.println("Done executing multi threading");
     }
 }
